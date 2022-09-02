c2cc87a04bfa20cb5a1d57f7b41d04d02f8fc212
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-09-01 19:05:15 -0700

    Added program to search

--- BinarySearchTree/BinarySearchTree/Program.cs
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -9,6 +9,7 @@ namespace BinarySearchTree
             Console.WriteLine("Hello, Welcome to Binary Search Tree!");
             Console.WriteLine("Enter 1 to create binary search tree");
             Console.WriteLine("Enter 2 to show binary search tree");
+            Console.WriteLine("Enter 3 to shearch in binary search tree");
             switch (Convert.ToInt32(Console.ReadLine()))
             {
                 case 1:
@@ -27,6 +28,19 @@ namespace BinarySearchTree
                     }
                     binaryTree.DisplayBST(binaryTree.root);
                     Console.WriteLine("\n Size of Binary Search Tree: " + binaryTree.Size(binaryTree.root));
+                    break;
+                case 3:
+                    ShearchBinarySearchTree<int> binarysTree = new ShearchBinarySearchTree<int>();                    
+                    int[] array = { 56, 30, 70, 22, 60, 40, 95, 16, 63, 67, 4, 11, 65 };                  
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        binarysTree.Insert(array[i]);
+                    }
+                    binarysTree.DisplayBST(binarysTree.root);
+                    Console.WriteLine("\n Size of Binary Search Tree: " + binarysTree.Size(binarysTree.root));                    
+                    Console.WriteLine();
+                    Console.WriteLine("Searching for Element 63 in the Binary Search Tree");
+                    binarysTree.Search(63);
                     break;
                 default:
                     Console.WriteLine("Enter valid number");
---
+++ BinarySearchTree/BinarySearchTree/ShearchBinarySearchTree.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+   
+        class ShearchBinarySearchTree<T> where T : IComparable
+        {           
+            public Node<T> root;            
+            public ShearchBinarySearchTree()
+            {
+                this.root = null;
+            }            
+            public void Insert(T data)
+            {
+                Node<T> newNode = new Node<T>(data);
+                if (root == null)
+                {
+                    root = newNode;
+                }
+                else
+                {               
+                    Node<T> current = root;                    
+                    Node<T> parent;
+                    while (true)
+                    {
+                        parent = current;                     
+                        if (data.CompareTo(current.data) < 0)
+                        {
+                            current = current.Left;
+                            if (current == null)
+                            {
+                                parent.Left = newNode;
+                                break;
+                            }
+                        }                        
+                        else
+                        {
+                            current = current.Right;
+                            if (current == null)
+                            {
+                                parent.Right = newNode;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }              
+            public void DisplayBST(Node<T> parent)
+            {
+                Console.WriteLine("Parent : {0}", parent.data);
+                if (parent.Left != null)
+                {
+                    Console.WriteLine("Left :" + parent.Left.data);
+                }
+                if (parent.Right != null)
+                {
+                    Console.WriteLine("Right :" + parent.Right.data);
+                }
+                if (parent.Left != null)
+                {
+                    DisplayBST(parent.Left);
+                }
+                if (parent.Right != null)
+                {
+                    DisplayBST(parent.Right);
+                }
+            }
+            public int Size(Node<T> node)
+            {
+                if (node == null)
+                {
+                    return 0;
+                }
+                else
+                {                    
+                    return (Size(node.Left) + 1 + Size(node.Right));
+                }
+            }
+            public void Search(T element)
+            {                
+                Node<T> current = root;
+                bool found = false;
+                while (current != null)
+                {                    
+                    if (current.data.Equals(element))
+                    {
+                        found = true;
+                        break;
+                    }
+                    else
+                    {                        
+                        if (element.CompareTo(current.data) < 0)
+                        {
+                            current = current.Left;
+                        }                   
+                        else
+                        {
+                            current = current.Right;
+                        }
+                    }
+                }
+                if (found)
+                    Console.WriteLine("Element Found");
+                else
+                    Console.WriteLine("Element Not Found");
+            }
+        }
+    
+}
